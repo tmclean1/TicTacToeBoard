8f3fd0153487435aa74da3b530afff0200085036
tmclean1 <tmclean1@mail.csuchico.edu>
2019-05-02 18:40:56 -0700

    Files added, functions filled out, testing povided
    All empty functions in TicTacToeBoard.cpp have been filled out to add functionality to the program. All functions, lines, and branches fully tested through the 29 test cases in TicTacToeBoardTest.cpp

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -19,7 +19,13 @@ TicTacToeBoard::TicTacToeBoard()
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+  if(turn == X){
+    turn = O;  
+  }
+  else{
+    turn = X;
+  }
+  return turn;
 }
 
 /**
@@ -33,7 +39,16 @@ Piece TicTacToeBoard::toggleTurn()
 **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
-  return Invalid;
+  if(getPiece(row, column) == Invalid)
+    return Invalid;
+  else{
+    if(getPiece(row, column) == Blank){
+      board[row][column] = turn;
+      return toggleTurn();
+    }
+    else
+      return board[row][column];
+  }
 }
 
 /**
@@ -42,7 +57,10 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
-  return Invalid;
+  if((row > -1 && row < 4) && (column > -1 && column < 4))
+    return board[row][column];
+  else
+    return Invalid;
 }
 
 /**
@@ -51,5 +69,29 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      if (board[i][j] == Blank)
+        return Invalid;
+    }
+  }
+  if((board[0][0] == board[1][0]) && (board[0][0] == board[2][0]))
+    return board[0][0];
+  else if((board[0][0] == board[1][1]) && (board[0][0] == board[2][2]))
+    return board[0][0];
+  else if((board[0][0] == board[0][1]) && (board[0][0] == board[0][2]))
+    return board[0][0];
+  else if((board[1][1] == board[1][0]) && (board[1][1] == board[1][2]))
+    return board[1][1];
+  else if((board[0][1] == board[1][1]) && (board[0][1] == board[2][1]))
+    return board[0][1];
+  else if((board[0][2] == board[1][2]) && (board[0][2] == board[2][2]))
+    return board[0][2];
+  else if((board[2][2] == board[2][1]) && (board[2][2] == board[2][0]))
+    return board[2][2];
+  else if((board[2][0] == board[1][1]) && (board[2][0] == board[0][2]))
+    return board[2][0];
+  else
+    return Blank;
+    
 }
--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -20,3 +20,300 @@ TEST(TicTacToeBoardTest, unitTestName)
 	ASSERT_TRUE(true);
 }
 */
+
+TEST( TicTacToeBoardTest, getpieceBlank)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.getPiece(0,0) == Blank);
+}
+
+TEST( TicTacToeBoardTest, getpieceInvalidoverMax)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.getPiece(4,4) == Invalid);
+}
+
+TEST( TicTacToeBoardTest, getpieceInvalidUnderMin)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.getPiece(-1,-1) == Invalid);
+}
+
+TEST( TicTacToeBoardTest, placingPieceReplacesBlank)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,0);
+	ASSERT_TRUE(myboard.getPiece(0,0) != Blank);
+}
+
+
+TEST( TicTacToeBoardTest, placingPieceInvalidoverMax)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.placePiece(4,4) == Invalid);
+}
+
+TEST( TicTacToeBoardTest, placingPieceInvalidUnderMin)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.placePiece(-2,-2) == Invalid);
+}
+
+TEST( TicTacToeBoardTest, placingPieceInvalidUnderMinRowOverMaxColumn)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.placePiece(-2, 5) == Invalid);
+}
+
+TEST( TicTacToeBoardTest, placingPieceInvalidUnderMinColumnOverMaxRow)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.placePiece(7 ,-2) == Invalid);
+}
+
+TEST( TicTacToeBoardTest, placingPieceInvalidRowOverMaxColumnInRange)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.placePiece(7 , 2) == Invalid);
+}
+
+TEST( TicTacToeBoardTest, placingPieceInvalidRowInRangeColumnUnderMin)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.placePiece(2 ,-2) == Invalid);
+}
+
+TEST( TicTacToeBoardTest, placingPieceInvalidColumnOverMaxRowInRange)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.placePiece(2 , 7) == Invalid);
+}
+
+TEST( TicTacToeBoardTest, placingPieceInvalidRowUnderMinColumnInRange)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.placePiece(-2 , 2) == Invalid);
+}
+
+
+TEST( TicTacToeBoardTest, togglesfromX)
+{
+	TicTacToeBoard myboard;
+	myboard.toggleTurn();
+	myboard.placePiece(0, 0);
+	ASSERT_TRUE(myboard.getPiece(0,0) == O);
+}
+
+
+TEST( TicTacToeBoardTest, togglesfromO)
+{
+	TicTacToeBoard myboard;
+	myboard.toggleTurn();
+	myboard.toggleTurn();
+	myboard.placePiece(1, 1);
+	ASSERT_TRUE(myboard.getPiece(1,1) == X);
+}
+
+
+TEST( TicTacToeBoardTest, placingPieceOverExistingPiece)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,0);
+	ASSERT_TRUE(myboard.placePiece(0,0) == X);
+}
+
+
+TEST( TicTacToeBoardTest, placingPieceTogglesPiece)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,0);
+	myboard.placePiece(1,1);
+	ASSERT_TRUE(myboard.getPiece(1,1) == O);
+}
+
+
+TEST( TicTacToeBoardTest, winnerInvalidIfboardEmpty)
+{
+	TicTacToeBoard myboard;
+	ASSERT_TRUE(myboard.getWinner() == Invalid);
+}
+
+TEST( TicTacToeBoardTest, WinnerInvalidIfBoardNotFull)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(1,1);
+	myboard.placePiece(1,2);
+	myboard.placePiece(0,1);
+	myboard.placePiece(1,0);
+	myboard.placePiece(2,1);
+	myboard.placePiece(2,0);
+	myboard.placePiece(0,0);
+	myboard.placePiece(0,2);
+	ASSERT_TRUE(myboard.getWinner() == Invalid);
+}
+
+
+TEST( TicTacToeBoardTest, XWinsDiagonalRight)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,0);
+	myboard.placePiece(1,0);
+	myboard.placePiece(2,0);
+	myboard.placePiece(0,1);
+	myboard.placePiece(1,1);
+	myboard.placePiece(2,1);
+	myboard.placePiece(0,2);
+	myboard.placePiece(2,2);
+	myboard.placePiece(1,2);
+	ASSERT_TRUE(myboard.getWinner() == X);
+}
+
+TEST( TicTacToeBoardTest, OWinsDiagonalRight)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,0);
+	myboard.placePiece(1,1);
+	myboard.placePiece(2,2);
+	myboard.placePiece(2,0);
+	myboard.placePiece(1,0);
+	myboard.placePiece(0,1);
+	myboard.placePiece(1,2);
+	myboard.placePiece(0,2);
+	myboard.placePiece(2,1);
+	ASSERT_TRUE(myboard.getWinner() == O);
+}
+
+TEST( TicTacToeBoardTest, NoWinners)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(1,1);
+	myboard.placePiece(2,0);
+	myboard.placePiece(0,0);
+	myboard.placePiece(1,0);
+	myboard.placePiece(0,2);
+	myboard.placePiece(0,1);
+	myboard.placePiece(1,2);
+	myboard.placePiece(2,2);
+	myboard.placePiece(2,1);
+	ASSERT_TRUE(myboard.getWinner() == Blank);
+}
+
+TEST( TicTacToeBoardTest, XWinsHorizontllyTop)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,0);
+	myboard.placePiece(0,1);
+	myboard.placePiece(1,0);
+	myboard.placePiece(1,1);
+	myboard.placePiece(2,0);
+	myboard.placePiece(2,2);
+	myboard.placePiece(0,2);
+	myboard.placePiece(1,2);
+	myboard.placePiece(2,1);
+	ASSERT_TRUE(myboard.getWinner() == X);
+}
+
+TEST( TicTacToeBoardTest, XWinsHorizontllyMiddle)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(1,1);
+	myboard.placePiece(1,0);
+	myboard.placePiece(2,0);
+	myboard.placePiece(0,2);
+	myboard.placePiece(0,1);
+	myboard.placePiece(2,2);
+	myboard.placePiece(1,2);
+	myboard.placePiece(0,0);
+	myboard.placePiece(2,1);
+	ASSERT_TRUE(myboard.getWinner() == X);
+}
+
+TEST( TicTacToeBoardTest, XWinsHorizontllyBottom)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,2);
+	myboard.placePiece(1,1);
+	myboard.placePiece(1,2);
+	myboard.placePiece(0,0);
+	myboard.placePiece(2,2);
+	myboard.placePiece(2,0);
+	myboard.placePiece(1,0);
+	myboard.placePiece(0,1);
+	myboard.placePiece(2,1);
+	ASSERT_TRUE(myboard.getWinner() == X);
+}
+
+TEST( TicTacToeBoardTest, XWinsDiagonallyLeft)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,0);
+	myboard.placePiece(0,2);
+	myboard.placePiece(1,1);
+	myboard.placePiece(1,0);
+	myboard.placePiece(2,1);
+	myboard.placePiece(0,1);
+	myboard.placePiece(2,0);
+	myboard.placePiece(1,2);
+	myboard.placePiece(2,2);
+	ASSERT_TRUE(myboard.getWinner() == X);
+}
+
+TEST( TicTacToeBoardTest, OWinsDiagonalLeft)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,1);
+	myboard.placePiece(0,0);
+	myboard.placePiece(1,2);
+	myboard.placePiece(1,0);
+	myboard.placePiece(1,1);
+	myboard.placePiece(2,1);
+	myboard.placePiece(2,0);
+	myboard.placePiece(0,2);
+	myboard.placePiece(2,2);
+	ASSERT_FALSE(myboard.getWinner() == O);
+}
+
+TEST( TicTacToeBoardTest, XWinsVerticallyLeft)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(0,0);
+	myboard.placePiece(2,0);
+	myboard.placePiece(0,1);
+	myboard.placePiece(2,1);
+	myboard.placePiece(2,2);
+	myboard.placePiece(1,1);
+	myboard.placePiece(1,0);
+	myboard.placePiece(1,2);
+	myboard.placePiece(0,2);
+	ASSERT_TRUE(myboard.getWinner() == X);
+}
+
+TEST( TicTacToeBoardTest, XWinsVerticallyMiddle)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(1,0);
+	myboard.placePiece(0,0);
+	myboard.placePiece(1,1);
+	myboard.placePiece(0,1);
+	myboard.placePiece(0,2);
+	myboard.placePiece(2,2);
+	myboard.placePiece(2,1);
+	myboard.placePiece(2,0);
+	myboard.placePiece(1,2);
+	ASSERT_TRUE(myboard.getWinner() == X);
+}
+
+TEST( TicTacToeBoardTest, XWinsVerticallyRight)
+{
+	TicTacToeBoard myboard;
+	myboard.placePiece(2,0);
+	myboard.placePiece(1,0);
+	myboard.placePiece(2,1);
+	myboard.placePiece(0,2);
+	myboard.placePiece(1,2);
+	myboard.placePiece(0,0);
+	myboard.placePiece(0,1);
+	myboard.placePiece(1,1);
+	myboard.placePiece(2,2);
+	ASSERT_TRUE(myboard.getWinner() == X);
+}
